950b8b590b205e54408afb3ea919f66dc048a6f4
Rohith Jayawardene <gambol99@gmail.com>
2024-10-12 17:26:20 +0100

    chore: removing the unrequired field

--- main.tf
+++ main.tf
@@ -8,40 +8,35 @@ resource "aws_organizations_organization" "organization" {
 
 ## Provision the organization units from the organization 
 resource "aws_organizations_organizational_unit" "level_1_ous" {
-  for_each     = { for record in local.level_1_ou_arguments : record.key => record }
-  name         = each.value.name
-  default_tags = var.tags
-  parent_id    = aws_organizations_organization.organization.roots[0].id
+  for_each  = { for record in local.level_1_ou_arguments : record.key => record }
+  name      = each.value.name
+  parent_id = aws_organizations_organization.organization.roots[0].id
 }
 
 ## Provision the organization units from the organization at level 2 
 resource "aws_organizations_organizational_unit" "level_2_ous" {
-  for_each     = { for record in local.level_2_ou_arguments : record.key => record }
-  name         = each.value.name
-  default_tags = var.tags
-  parent_id    = aws_organizations_organizational_unit.level_1_ous[each.value.parent].id
+  for_each  = { for record in local.level_2_ou_arguments : record.key => record }
+  name      = each.value.name
+  parent_id = aws_organizations_organizational_unit.level_1_ous[each.value.parent].id
 }
 
 ## Provision the organization units from the organization at level 3
 resource "aws_organizations_organizational_unit" "level_3_ous" {
-  for_each     = { for record in local.level_3_ou_arguments : record.key => record }
-  name         = each.value.name
-  default_tags = var.tags
-  parent_id    = aws_organizations_organizational_unit.level_2_ous[each.value.parent].id
+  for_each  = { for record in local.level_3_ou_arguments : record.key => record }
+  name      = each.value.name
+  parent_id = aws_organizations_organizational_unit.level_2_ous[each.value.parent].id
 }
 
 ## Provision the organization units from the organization at level 4 
 resource "aws_organizations_organizational_unit" "level_4_ous" {
-  for_each     = { for record in local.level_4_ou_arguments : record.key => record }
-  name         = each.value.name
-  default_tags = var.tags
-  parent_id    = aws_organizations_organizational_unit.level_3_ous[each.value.parent].id
+  for_each  = { for record in local.level_4_ou_arguments : record.key => record }
+  name      = each.value.name
+  parent_id = aws_organizations_organizational_unit.level_3_ous[each.value.parent].id
 }
 
 ## Provision the organization units from the organization at level 5 
 resource "aws_organizations_organizational_unit" "level_5_ous" {
-  for_each     = { for record in local.level_5_ou_arguments : record.key => record }
-  name         = each.value.name
-  default_tags = var.tags
-  parent_id    = aws_organizations_organizational_unit.level_4_ous[each.value.parent].id
+  for_each  = { for record in local.level_5_ou_arguments : record.key => record }
+  name      = each.value.name
+  parent_id = aws_organizations_organizational_unit.level_4_ous[each.value.parent].id
 }
