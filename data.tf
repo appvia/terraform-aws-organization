e1c9ae364ddefb90105907781acb15f5bf2e5fc3
Rohith Jayawardene <gambol99@gmail.com>
2024-12-05 15:50:37 +0000

    chore: dropping the unrequired data lookup

--- data.tf
+++ data.tf
@@ -3,9 +3,3 @@
 data "aws_organizations_organizational_unit_descendant_accounts" "current" {
   parent_id = aws_organizations_organization.organization.roots[0].id
 }
-
-## Lookup up and build a map of all the organizational units within the organization
-## using the keys as the organizational unit hirearchy key
-data "aws_organizations_organizational_units" "current" {
-  parent_id = aws_organizations_organization.organization.roots[0].id
-}
