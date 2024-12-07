4a8270759cf8bf003dec603cac1d5ec737e43760
Rohith Jayawardene <gambol99@gmail.com>
2024-12-07 08:35:34 +0000

    feat: adding the resource control policy to the default enabled organizational policies

--- variables.tf
+++ variables.tf
@@ -59,6 +59,7 @@ variable "enable_policy_types" {
   default = [
     "AISERVICES_OPT_OUT_POLICY",
     "BACKUP_POLICY",
+    "RESOURCE_CONTROL_POLICY",
     "SERVICE_CONTROL_POLICY",
     "TAG_POLICY",
   ]
